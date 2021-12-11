64ab282cb33205bf03a02bfbdb2f350f1bc5b345
Dexstel <wlmsux@gmail.com>
2021-12-11 22:35:24 +0300

    Added sorted integer array.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// Set two random arrays for checking both int and generic functions
+
+int[] Integer = new int[500];
+for(int i = 0; i < Integer.Length; i++)
+{
+    Integer[i] = i;
+}
+
+
